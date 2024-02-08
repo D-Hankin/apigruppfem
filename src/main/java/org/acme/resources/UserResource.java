260c24c702825a911ab6d56a750d9548b5c40cfc
David Hankin <d_hankin@outlook.com>
2024-02-08 14:09:18 +0100

    successful get from postman (getAllUSers)

--- src/main/java/org/acme/resources/UserResource.java
+++ src/main/java/org/acme/resources/UserResource.java
@@ -3,11 +3,18 @@ package org.acme.resources;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 
+import java.util.List;
+
+import org.acme.model.User;
 import org.acme.services.UserService;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/api/city/")
 @Produces(MediaType.APPLICATION_JSON)
@@ -17,6 +24,21 @@ public class UserResource {
     @Inject
     UserService userService;
 
-    
+    @GET
+    @Operation(summary = "Show all the current users", description = "Retrieve and show all the users currently in the database.")
+    @APIResponse(
+        responseCode = "204",
+        description = "No user currently in the database"
+    )
+    public Response getUsers() {
+
+        List<User> users = userService.findAll();
+
+        if (users.isEmpty()) {
+            return Response.noContent().build();
+        }
+
+        return Response.ok(users).build();
+    }
     
 }
